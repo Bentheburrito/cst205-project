9855fc2fdfbfe42e46f846fd115403aee5f29d18
Yuki Okamoto <yuki.okamoto@Yukis-MacBook-Pro-2.local>
2023-05-03 15:33:53 -0700

    add: links sql and subject image_url

--- migrations/20230501-init-schema.sql
+++ migrations/20230501-init-schema.sql
@@ -30,11 +30,10 @@ CREATE TABLE questions (
 	option_2 varchar(250),
 	option_3 varchar(250),
 	answer integer,
-	image_url varchar(500),
 	foreign key (quiz_id) references quizzes(id)
 );
 
-INSERT INTO subjects (name, image_url) VALUES ('Marine Life', 'placeholder here');
+INSERT INTO subjects (name, image_url) VALUES ('Marine Life', 'https://t4.ftcdn.net/jpg/02/28/67/47/240_F_228674782_TjfB8augkK4l6NZxddFvlHHw2K7bVpbp.jpg');
 INSERT INTO quizzes (name, subject_id) VALUES ('Aquatic Animals', 1);
 
 INSERT INTO questions (quiz_id, question_text, option_1, option_2, option_3, answer) VALUES (1, 'Which of the following is the largest species of shark?', 'Hammerhead Shark', 'Great White Shark', 'Whale Shark', 3);
@@ -47,3 +46,14 @@ INSERT INTO questions (quiz_id, question_text, option_1, option_2, option_3, ans
 INSERT INTO questions (quiz_id, question_text, option_1, option_2, option_3, answer) VALUES (1, 'Which of the following is not a type of whale?', 'Humpback Whale', 'Sperm Whale', 'Giant Squid', 3);
 INSERT INTO questions (quiz_id, question_text, option_1, option_2, option_3, answer) VALUES (1, 'What aquatic animal has the ability to regenerate lost limbs?', 'Lobster', 'Octopus', 'Sea Cucumber', 2);
 INSERT INTO questions (quiz_id, question_text, option_1, option_2, option_3, answer) VALUES (1, 'What type of marine invertebrate is known for its stinging cells?', 'Coral', 'Jellyfish', 'Sea Anemone', 2);
+
+INSERT INTO links (quiz_id, name, url) VALUES (1, 'What is the Biggest Shark? A Chart Shows the Diversity of Shark Sizes | Smithsonian Ocean', 'https://ocean.si.edu/ocean-life/sharks-rays/what-biggest-shark-chart-shows-diversity-shark-sizes')
+INSERT INTO links (quiz_id, name, url) VALUES (1, 'Sea Turtle Diet — SEE Turtles', 'https://www.seeturtles.org/sea-turtle-diet')
+INSERT INTO links (quiz_id, name, url) VALUES (1, 'Fun Facts About One of the Most Intelligent Aquatic Mammals', 'https://maddcappgames.com/blogs/news/fun-facts-about-one-of-the-most-intelligent-aquatic-mammals')
+INSERT INTO links (quiz_id, name, url) VALUES (1, 'Seahorse Color-Color is Not Fixed and Changes', 'https://seahorsesavvy.com/blogs/news/seahorse-color-color-is-not-fixed-and-changes')
+INSERT INTO links (quiz_id, name, url) VALUES (1, 'Are starfish really fish?', 'https://oceanservice.noaa.gov/facts/starfish.html')
+INSERT INTO links (quiz_id, name, url) VALUES (1, 'The Sailfish is the Fastest Fish in the World - Ocean Conservancy', 'https://oceanconservancy.org/blog/2021/10/28/sailfish-fastest-fish/')
+INSERT INTO links (quiz_id, name, url) VALUES (1, 'Penguin Fact Sheet | Blog | Nature | PBS', 'https://www.pbs.org/wnet/nature/blog/penguin-fact-sheet/')
+INSERT INTO links (quiz_id, name, url) VALUES (1, 'Humpback Whale vs Sperm Whale: What are the Differences? - AZ Animals', 'https://a-z-animals.com/blog/humpback-whale-vs-sperm-whale-what-are-the-differences/')
+INSERT INTO links (quiz_id, name, url) VALUES (1, 'TIL: How Spiny Sea Creatures Regrow Body Parts', 'https://education.nationalgeographic.org/resource/til-how-spiny-sea-creatures-regrow-body-parts/')
+INSERT INTO links (quiz_id, name, url) VALUES (1, 'Phylum Cnidaria', 'https://manoa.hawaii.edu/exploringourfluidearth/biological/invertebrates/phylum-cnidaria')
